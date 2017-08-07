c96f27a32a82ea626836b3e39a7ab3dec9efe440
Zehef Molotov <ophois34@gmail.com>
2017-08-07 16:55:14 +0200

    07/08/17
    -Mise en Place du Core
    -Mise en Place de App et News Controller
    -Autochargement des Classes avec SPL AUTOLOADER
    -Bootstrap

---
+++ 02_TechNews/public/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+# Affichage des Erreurs en mode DEV = 1 , en PROD = 0
+ini_set('display_errors',1); // 1 = activé , 0 = désactivé
+
+# Racine du Site Internet
+# Nécessite PHP 7+
+# http://php.net/manual/fr/function.dirname.php
+
+define('RACINE_SITE',dirname(__FILE__,2));
+
+# Chemin Relatif vers le Dossier Public
+define('PUBLIC_URL','/POO-Hugo/02_TechNews/public');
+
+# En-tete du site
+define('HEADER_SITE', RACINE_SITE.'/Application/Layout/header.inc.php');
+
+# Pied de page du site
+define('FOOTER_SITE', RACINE_SITE.'/Application/Layout/footer.inc.php');
+
+# Les Vues
+define('VIEW_SITE', RACINE_SITE.'/Application/Views');
+
+# Autoloader
+require_once 'Autoloader.php';
+Autoloader::register();
